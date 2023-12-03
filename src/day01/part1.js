b8cac8bcd0168fa3802ef979ca2f80033ed5f375
Nicolas <73860549+nico430@users.noreply.github.com>
2023-12-03 17:52:56 +0000

    OK: part1 finished

--- src/day01/part1.js
+++ src/day01/part1.js
@@ -1,5 +1,27 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input) {
-  return 0;
+  let lines = input.split("\n");
+  let decodedNumbers = [];
+
+  lines.forEach(line => {
+    let lineNumbers = []
+    line.split("").forEach(char=>{
+      let parsed = parseInt(char)
+      if(!isNaN(parsed)){
+        lineNumbers.push(parsed)
+      }
+    })
+    let parsedLine = parseInt([ lineNumbers[0], lineNumbers[lineNumbers.length-1] ].join("")) // a1b2c3d4e5f -> 1, 2, 3, 4, 5 -> 15
+    if (!isNaN(parsedLine)){
+      decodedNumbers.push(parsedLine)
+    }
+  });
+  let sum = decodedNumbers.reduce(
+    (acumulator, current)=>{
+      //console.log(acumulator, current)
+      return acumulator + current
+    },0
+  )
+  return sum;
 }
